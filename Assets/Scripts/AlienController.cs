becadf7d383d72b5804e35647f51f82c69cb484f
kokostahr <starking.nn@gmail.com>
2024-10-11 09:32:36 +0200

    LOL the dropped enemies

--- Assets/Scripts/AlienController.cs
+++ Assets/Scripts/AlienController.cs
@@ -7,6 +7,18 @@ public class AlienController : MonoBehaviour
     //Script to make the alien move differently!
     //Reference the countdown timer script, then use time remaining and anim parametre (float)
     //to set different values for the different alien states, and say when 
+
+    [Header("TIMER LINKING")]
+    [Space(5)]
+    CountDownTimer countDownTimer;
+
+   
+
+    private void Awake()
+    {
+        
+    }
+
     void Start()
     {
         
