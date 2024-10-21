95db4169a4324b8069fa05627595e0bd6f8f2ab0
spiceygabs <sisigabz@gmail.com>
2024-10-21 02:55:25 +0200

    Updated aiAgent.cs code! :0

--- Assets/Scripts/Enemies/aiAgent.cs
+++ Assets/Scripts/Enemies/aiAgent.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class aiAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+   private NavMeshAgent agent;
+    [SerializeField] private Transform movePos;
+    private void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+       agent.SetDestination(movePos.position);    
     }
 }
